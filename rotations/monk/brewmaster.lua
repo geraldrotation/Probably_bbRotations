91af9e170f79e2740a02f017e19e42b3a2ad7af4
Backburn <jankster@gmail.com>
2014-01-20 23:59:04 -0800

    add brewmaster monk

--- rotations/monk/brewmaster.lua
+++ rotations/monk/brewmaster.lua
@@ -1,116 +1,94 @@
--- SPEC ID 268 Brewmaster
-ProbablyEngine.rotation.register(268, {
+-- ProbablyEngine Rotation Packager
+-- Custom Brewmaster Monk Rotation
+-- Created on Dec 25th 2013 1:00 am
+ProbablyEngine.rotation.register_custom(268, "bbBrewmasterMonk", {
+-- PLAYER CONTROLLED:
+-- SUGGESTED TALENTS:
+-- CONTROLS: Pause - Left Control
 
-  -------------------
-  -- Start Racials --
-  -------------------
-  
-  -- =^*~ Alliance ~*^= --
-  -- Dwarves
-  { "Stoneform", "player.health <= 65" },
- 
-  -- Humans
-  { "Every Man for Himself", "player.state.charm" },
-  { "Every Man for Himself", "player.state.fear" },
-  { "Every Man for Himself", "player.state.incapacitate" },
-  { "Every Man for Himself", "player.state.sleep" },
-  { "Every Man for Himself", "player.state.stun" },
- 
-  -- Draenei
-  { "Gift of the Naaru", "player.health <= 70", "player" },
- 
-  -- Gnomes
-  { "Escape Artist", "player.state.root" },
-  { "Escape Artist", "player.state.snare" },
+-- COMBAT
+	-- Rotation Utilities
+	{ "pause", "modifier.lcontrol" },
+	{ "pause", "@bbLib.bossMods" },
+	{ "pause", "player.buff(5384)" }, -- Feign Death
+	{ "pause", { "toggle.pvpmode", "@bbLib.BGFlag" } },
+	{ "/targetenemy [noexists]", { "toggle.autotarget", "!target.exists" } },
+	{ "/targetenemy [dead]", { "toggle.autotarget", "target.exists", "target.dead" } },
 
-  -- Night Elves
-  { "Shadowmeld", "target.threat >= 80" },
-  { "Shadowmeld", "focus.threat >= 80"},
-  
-  -- <,&~ Horde ~&,> --
-  -- Forsaken
-  { "Will of the Forsaken", "player.state.fear" },
-  { "Will of the Forsaken", "player.state.charm" },
-  { "Will of the Forsaken", "player.state.sleep" },
- 
-  -- _,-=%Pandaren%=-,_ --
-  { "Quaking Palm", "modifier.interrupts" },
-  
-  -----------------
-  -- End Racials --
-  -----------------
-  
-  --------------------
-  -- Start Rotation --
-  --------------------
-  
-  -- Get Fucked Button
-  { "Touch of Death", "player.buff(Death Note)" },
-  
-  -- Threat Control
-  { "Provoke", "mouseover.threat < 100", "mouseover" },
-  
-  -- Survival
-  { "Expel Harm", "player.health < 95" },
-  { "Fortifying Brew", "player.health <= 50" },
-  { "Nimble Brew", "player.state.fear" },
-  { "Nimble Brew", "player.state.stun" },
-  { "Nimble Brew", "player.state.root" },
-  { "Nimble Brew", "player.state.horror" },
-  { "Dampen Harm", "player.health <= 60" },
-  { "Diffuse Magic", "player.health <= 60" },
-  
-  -- Multitarget
-  { "Rushing Jade Wind", { "modifier.multitarget", "!player.buff(Rushing Jade Wind)", "!player.buff(Rushing Jade Wind).duration < 2" } },
-  { "Spinning Crane Kick", "modifier.multitarget" },
-  
-  -- Ground Stuff
-  { "Dizzying Haze", "modifier.shift", "ground" },
-  { "Summon Black Ox Statue", "modifier.control", "ground" },
+	-- Racials
+	{ "Stoneform", "player.health <= 65" },
+	{ "Every Man for Himself", "player.state.charm" },
+	{ "Every Man for Himself", "player.state.fear" },
+	{ "Every Man for Himself", "player.state.incapacitate" },
+	{ "Every Man for Himself", "player.state.sleep" },
+	{ "Every Man for Himself", "player.state.stun" },
 
-  -- Interrupts
-  { "Spear Hand Strike", "modifier.interrupts" },
-  { "Grapple Weapon", "modifier.interrupts" },
-  { "Leg Sweep", "modifier.interrupts", "target.range <= 5" },
+	-- Off GCD
+	{ "Touch of Death", "player.buff(Death Note)" },
 
-  -- Talents
-  -- { "Chi Wave", "target" },
-  -- { "Zen Sphere", "!player.buff(Zen Sphere)", "player" },
-  -- { "Chi Burst", "!player.moving" },
-  { "Invoke Xuen, the White Tiger" },
-  { "Tiger's Lust", { "target.enemy", "target.range >= 15" }, "player" },
+	{ "Provoke", { "toggle.autotaunt", "@bbLib.bossTaunt" } },
 
-  -- Brews
-  { "Purifying Brew", "player.debuff(Moderate Stagger)" },
-  { "Purifying Brew", "player.debuff(Heavy Stagger)" },
-  { "Elusive Brew", "player.buff(Elusive Brew).count >= 10" },
+	-- Survival
+	{ "Expel Harm", "player.health < 95" },
+	{ "Fortifying Brew", "player.health <= 50" },
+	{ "Nimble Brew", "player.state.fear" },
+	{ "Nimble Brew", "player.state.stun" },
+	{ "Nimble Brew", "player.state.root" },
+	{ "Nimble Brew", "player.state.horror" },
+	{ "Dampen Harm", "player.health <= 60" },
+	{ "Diffuse Magic", "player.health <= 60" },
 
-  -- Rotation
-  { "Keg Smash" },
-  { "Guard", "player.buff(Power Guard)" },
-  { "Breath of Fire", { "target.debuff(Dizzying Haze)", "!target.debuff(Breath of Fire)"} },
-  { "Tiger Palm", "player.buff(Tiger Power).duration < 4" },
-  { "Blackout Kick" },
-  { "Jab" },
+	-- Multitarget
+	{ "Rushing Jade Wind", { "modifier.multitarget", "!player.buff(Rushing Jade Wind)", "!player.buff(Rushing Jade Wind).duration < 2" } },
+	{ "Spinning Crane Kick", "modifier.multitarget" },
 
-  ------------------
-  -- End Rotation --
-  ------------------
-  
+	-- Ground Stuff
+	{ "Dizzying Haze", "modifier.shift", "ground" },
+	{ "Summon Black Ox Statue", "modifier.control", "ground" },
+	{ "Healing Sphere", "modifer.alt", "ground" },
+
+	-- Interrupts
+	{ "Spear Hand Strike", "modifier.interrupts" },
+	{ "Grapple Weapon", "modifier.interrupts" },
+	{ "Leg Sweep", "modifier.interrupts", "target.range <= 10" },
+
+	-- Talents
+	{ "Chi Wave" },
+	{ "Zen Sphere", "!player.buff(Zen Sphere)", "player" },
+	{ "Chi Burst" },
+	{ "Invoke Xuen, the White Tiger" },
+	{ "Tiger's Lust", "target.range >= 15" },
+
+	-- Brews
+	{ "Purifying Brew", "player.debuff(Moderate Stagger)" },
+	{ "Purifying Brew", "player.debuff(Heavy Stagger)" },
+	{ "Elusive Brew", "player.buff(Elusive Brew).count >= 10" },
+
+	-- Rotation
+	{ "Keg Smash" },
+	{ "Guard", "player.buff(Power Guard)" },
+
+	{ "Breath of Fire", { "target.debuff(Dizzying Haze)", "!target.debuff(Breath of Fire)" } },
+
+	{ "Tiger Palm", "player.buff(Tiger Power).duration < 4" },
+	{ "Blackout Kick" },
+	{ "Jab" },
+	{ "Tiger Palm" },
+
   },{
-  
-  ---------------
-  -- OOC Begin --
-  ---------------
-  
-  -- Buffs
-  { "Legacy of the Emperor", "!player.buff(Legacy of the Emperor)" },
-  
-  -- Ground Stuff
-  { "Dizzying Haze", "modifier.shift", "ground" },
-  { "Summon Black Ox Statue", "modifier.control", "ground" },
+-- OUT OF COMBAT
+	-- Buffs
+	{ "Legacy of the Emperor", "!player.buff(Legacy of the Emperor).any" },
 
-  -------------
-  -- OOC End --
-  -------------
-})
+	-- Ground Stuff
+	{ "Dizzying Haze", "modifier.shift", "ground" },
+	{ "Summon Black Ox Statue", "modifier.control", "ground" },
+
+},
+function()
+	ProbablyEngine.toggle.create('mouseovers', 'Interface\\Icons\\inv_pet_lilsmoky', 'Use Mouseovers', 'Automatically cast spells on mouseover targets')
+	ProbablyEngine.toggle.create('pvpmode', 'Interface\\Icons\\achievement_pvp_o_h', 'Enable PvP', 'Toggle the usage of PvP abilities.')
+	ProbablyEngine.toggle.create('limitaoe', 'Interface\\Icons\\spell_fire_flameshock', 'Limit AoE', 'Toggle to avoid using CC breaking aoe effects.')
+	ProbablyEngine.toggle.create('autotarget', 'Interface\\Icons\\ability_hunter_snipershot', 'Auto Target', 'Automaticaly target the nearest enemy when target dies or does not exist.')
+	ProbablyEngine.toggle.create('autotaunt', 'Interface\\Icons\\spell_nature_reincarnation', 'Auto Taunt', 'Automaticaly taunt the boss at the appropriate stacks')
+end)
